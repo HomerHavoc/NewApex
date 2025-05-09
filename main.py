b6791a5980ed8c3ad69e73bbe458371de9a04a3c
HomerHavoc <b9kmegaways@gmail.com>
2025-05-09 03:03:42 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
 import os
 import sys
 
+# Ensure GitHub Actions runner can find local modules
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 from apex_engine.main_engine import run_daily_predictions
 from export_predictions import export_to_csv
 
 
-def main():
+if __name__ == '__main__':
     run_daily_predictions()
     export_to_csv()
-
-
-if __name__ == '__main__':
-    main()
